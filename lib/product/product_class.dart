476f9d9336917e620ee0f28565240be6eb19f5c7
to_do_app <eodnjs748@gmail.com>
2025-10-27 20:45:31 +0900

    productclass생성

---
+++ lib/product/product_class.dart
@@ -0,0 +1,13 @@
+class Product {
+  String image;
+  String name;
+  String description;
+  int price;
+
+  Product({
+    required this.image,
+    required this.name,
+    required this.description,
+    required this.price,
+  });
+}
