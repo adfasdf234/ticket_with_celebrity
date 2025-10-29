395299e319f17b210fdc20d5745548fcbcf4ec11
jm <joy020926@gmail.com>
2025-10-29 20:40:43 +0900

    feat: add_page(text_filed, elevated_button)

--- lib/screen/product_add_page.dart
+++ lib/screen/product_add_page.dart
@@ -27,20 +27,131 @@ class _MyWidgetState extends State<ProductAdd> {
         elevation: 0,
         shape: Border(bottom: BorderSide(color: Color(0xFFFF77A9), width: 1.0)),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: double.infinity,
+              height: 250,
+              child: Image.asset('asset/image/lee.webp', fit: BoxFit.cover),
             ),
-          ),
-          Container(
-            height: 300,
-            width: double.infinity,
-            child: Image.asset('assets/image/kim.webp.jpg', fit: BoxFit.cover),
-          ),
-          SizedBox()
-        ],
+            SizedBox(height: 20),
+            Row(
+              children: [
+                SizedBox(width: 10),
+                Text(
+                  '상품이름',
+                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+                ),
+                Spacer(),
+                SizedBox(
+                  height: 48,
+                  width: 250,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      // labelText: '',
+                      hintText: '상품이름을 입력해주세요',
+                      labelStyle: TextStyle(color: Colors.black),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        borderSide: BorderSide(width: 1, color: Colors.black),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      ),
+                    ),
+                    keyboardType: TextInputType.text,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              children: [
+                SizedBox(width: 10),
+                Text(
+                  '가격',
+                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+                ),
+                Spacer(),
+                SizedBox(
+                  height: 48,
+                  width: 250,
+                  // decoration: BoxDecoration(
+                  //   border: Border.all(),
+                  //   borderRadius: BorderRadius.circular(10),
+                  // ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      // labelText: '',
+                      hintText: '가격을 입력해주세요',
+                      labelStyle: TextStyle(color: Colors.black),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        borderSide: BorderSide(width: 1, color: Colors.black),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      ),
+                    ),
+                    keyboardType: TextInputType.number,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 15),
+            Row(
+              children: [
+                SizedBox(width: 10),
+                Text(
+                  '상품설명',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.black54,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 5),
+            Row(
+              children: [
+                SizedBox(width: 10),
+                Expanded(
+                  child: TextField(
+                    maxLines: 8,
+                    decoration: InputDecoration(
+                      hintText: '상품설명을 입력해주세요',
+                      labelStyle: TextStyle(color: Colors.black),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        borderSide: BorderSide(width: 1, color: Colors.black),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      ),
+                    ),
+                    keyboardType: TextInputType.text,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: SizedBox(
+        height: 65,
+        width: double.infinity,
+        child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero,),
+            backgroundColor: Color(0xFFFF77A9),
+            foregroundColor: Colors.white,
+          ),child: Text('상품등록하기', style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold))
+        ),
       ),
     );
   }
